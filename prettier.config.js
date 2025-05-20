59b9ddeae0ee769928447c774c5ba59c8f0ea4bd
luukleenders <luuk@digitalindividual.dev>
2025-05-20 22:59:57 +0200

    quick vite setup

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  tailwindFunctions: ['classNames'],
+  trailingComma: 'es5',
+};
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Luuk Leenders</h1>
+    </>
+  );
+}
+
+export default App;
